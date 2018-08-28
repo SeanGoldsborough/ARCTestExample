ad479285ef142ab742befa7f7d4d80362cc2c155
Sean Goldsborough <smgoldsborough@gmail.com>
2018-08-28 10:27:49 -0400

    feat: adds Person and MacBook classes

---
+++ ARCTestExample/Classes.swift
@@ -0,0 +1,37 @@
+//
+//  Classes.swift
+//  ARCTestExample
+//
+//  Created by Sean Goldsborough on 8/28/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    let name: String
+    var macbook: MacBook?
+    
+    init(name: String, macbook: MacBook?) {
+        self.name = name
+        self.macbook = macbook
+    }
+    
+    deinit {
+        print("name has been deinit-ed \(name)")
+    }
+}
+
+class MacBook {
+    let name: String
+    var owner: Person?
+    
+    init(name: String, owner: Person?) {
+        self.name = name
+        self.owner = owner
+    }
+    
+    deinit {
+        print("MacBook named: \(name) has been deinit-ed")
+    }
+}
